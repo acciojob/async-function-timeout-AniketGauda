52a3f6416bc66bf270e048e37b62ed8726a1adf0
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-07-14 02:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const promise = ()=>{
+    let text = document.getElementById("text").value;
+    let delay = document.getElementById("delay").value;
+
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(text);
+        },delay)
+    })
+}
+
+document.getElementById("btn").onclick = (e)=>{
+    e.preventDefault();
+    let prnt = document.getElementById("output");
+    async function display(){
+        const data = await promise();
+        prnt.innerHTML+=`
+            ${data}
+        `
+        // console.log(data);
+    }
+
+    display();
+}
